4d6c4996f6e1f7eafc5e1e3d80359b53bdde1a97
Abdessamad Abidar <142503084+abdessamadabidar@users.noreply.github.com>
2024-12-06 02:54:54 +0100

    Update jenkins shared library

--- src/Maven.groovy
+++ src/Maven.groovy
@@ -11,10 +11,10 @@ class Maven implements Serializable {
 
     def build(String directory) {
         script.sh "pwd"
-        script.sh "mvn -f /var/jenkins_home/workspace/shopino-microservices/services/${directory} clean package -DskipTests"
+        script.sh "mvn -f services/${directory} clean package -DskipTests"
     }
 
     def install(String directory) {
-        script.sh "mvn -f /var/jenkins_home/workspace/shopino-microservices/services/${directory} clean install -DskipTests"
+        script.sh "mvn -f services/${directory} clean install -DskipTests"
     }
 }
